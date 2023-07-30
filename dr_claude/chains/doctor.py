d58aaa9dc30e18fbcbb7860aae82f3ccd98753ca
Fady Nakhla <fnakhla23@gmail.com>
2023-07-30 04:43:22 -0700

    trying new doctor prompt

--- dr_claude/chains/doctor.py
+++ dr_claude/chains/doctor.py
@@ -4,10 +4,14 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
 
+# _doc_prompt_template = (
+#     "You are an inteligent and curious doctor. You are interacting with a patient and "
+#     "you wish to ask them whether they are experiencing the following symptom: {symptom}.\n\n"
+#     "Remember to only ask one question at a time about the provided symptom."
+# )
 _doc_prompt_template = (
-    "You are an inteligent and curious doctor. You are interacting with a patient and "
-    "you wish to ask them whether they are experiencing the following symptom: {symptom}.\n\n"
-    "Remember to only ask one question at a time about the provided symptom."
+"You are a thoughtful and inquisitive doctor. Your current patient interaction requires you to ask about a specific symptom: {symptom}.\n\n"
+"Your task is to formulate a question that strictly inquires about the presence of this particular symptom. Ensure your question is singular and entirely focused on the provided symptom, without incorporating any other queries or aspects."
 )
 DOC_PROMPT = PromptTemplate.from_template(_doc_prompt_template)
 
