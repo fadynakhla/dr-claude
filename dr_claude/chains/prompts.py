70c0b853f93570ee107471b0e6b88159244e6b21
Fady Nakhla <fnakhla23@gmail.com>
2023-07-29 17:55:11 -0700

    adding initial prompts

---
+++ dr_claude/chains/prompts.py
@@ -0,0 +1,23 @@
+from langchain.prompts import PromptTemplate
+
+
+_symptom_extract_template = """Given the following conversation:
+Question: {question}
+Response: {response}
+
+Please write out the medical symptoms that appear as well as whether they are present.
+
+Your response should be in the following format please do not include any other text:
+<symptom> symptom1 : yes </symptom>
+<symptom> symptom2 : no </symptom>
+"""
+
+_symptom_match_template = """Given the symptom: {symptom} which of the following retrievals is the best match?
+{retrievals}
+
+Select only one and write it below.
+"""
+
+
+SYMPTOM_EXTRACT_PROMPT = PromptTemplate.from_template(_symptom_extract_template)
+SYMPTOM_MATCH_PROMPT = PromptTemplate.from_template(_symptom_match_template)
