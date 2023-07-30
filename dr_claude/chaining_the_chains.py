dd241cc1fa873fb7510d509114a3a1bbf214f35b
Wian Stipp <stippwian@gmail.com>
2023-07-30 01:13:44 -0500

    hooking things together

--- dr_claude/chaining_the_chains.py
+++ dr_claude/chaining_the_chains.py
@@ -5,6 +5,7 @@ from dr_claude.chains import patient
 from dr_claude.chains import matcher
 from dr_claude.chains import prompts
 from dr_claude.retrieval import retriever
+from loguru import logger
 
 
 class ChainChainer:
@@ -15,7 +16,6 @@ class ChainChainer:
         symptoms: List[str],
     ) -> List[Dict[str, Any]]:
         self.patient_note = patient_note
-
         self.doc_chain = doctor.get_doc_chain()
         self.patient_chain = patient.get_patient_chain()
         self.matcher_chain = matcher.MatchingChain.from_anthropic(
@@ -28,15 +28,24 @@ class ChainChainer:
     def interaction(self, symptom: str):
         doc_inputs = {"symptom": symptom}
         doc_response = self.doc_chain(doc_inputs)["text"]
+        logger.info("Doc question: {}", doc_response)
         patient_inputs = {"medical_note": self.patient_note, "question": doc_response}
         patient_response = self.patient_chain(patient_inputs)["text"]
+        logger.info("Patient response: {}", patient_response)
         matcher_inputs = {"question": doc_response, "response": patient_response}
         return self.matcher_chain(matcher_inputs)
 
 
 if __name__ == "__main__":
     note = "Patient presents with abdominal cramps and nausea. He does not have a fever, but is feeling dizzy."
-    symptoms = ["fever", "cough", "headache", "sore throat", "runny nose", "abdominal cramps"]
+    symptoms = [
+        "fever",
+        "cough",
+        "headache",
+        "sore throat",
+        "runny nose",
+        "abdominal cramps",
+    ]
     retrieval_config = retriever.HuggingFaceEncoderEmbeddingsConfig(
         model_name_or_path="bert-base-uncased",
         device="cpu",
