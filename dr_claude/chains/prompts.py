5adc66cf213bec3d27420da8caa9c098b4ef0e9f
Fady Nakhla <fnakhla23@gmail.com>
2023-07-29 18:06:23 -0700

    updates

--- dr_claude/chains/prompts.py
+++ dr_claude/chains/prompts.py
@@ -15,7 +15,10 @@ Your response should be in the following format please do not include any other 
 _symptom_match_template = """Given the symptom: {symptom} which of the following retrievals is the best match?
 {retrievals}
 
-Select only one and write it below.
+Select only one and write it below in the following formatt:
+<match> retrieval2 </match>
+
+Do not include any other text.
 """
 
 
